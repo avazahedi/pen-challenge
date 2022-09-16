71ad3589fd84c2ea72f486c28fc83da9d8406679
Ava Zahedi <AvaZahedi2023@u.northwestern.edu>
2022-09-16 10:51:02 -0500

    added try/except for if purple pen is not in screen (cannot find contour)

--- cv_demos.py
+++ cv_demos.py
@@ -160,9 +160,12 @@ try:
             cv2.drawContours(images, [cnt], 0, (0,255,0), 3)
 
             M = cv2.moments(cnt)
-            centroid_x = int(M['m10']/M['m00'])
-            centroid_y = int(M['m01']/M['m00'])
-            cv2.circle(images, (centroid_x, centroid_y), 10, (255,0,0), -1)
+            try:
+                centroid_x = int(M['m10']/M['m00'])
+                centroid_y = int(M['m01']/M['m00'])
+                cv2.circle(images, (centroid_x, centroid_y), 10, (255,0,0), -1)
+            except:
+                print('Contour not found')
         # else:
         #     print('no contours found')
 
